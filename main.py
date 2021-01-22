49302073afd052a761bdf39f66a62453e96d18d1
Frostplexx <62436912+Frostplexx@users.noreply.github.com>
2021-01-22 15:34:16 +0100

    changed fonts

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import os
 
 # Choose font, in array, 0 is first font
 font = os.listdir("fonts/")[0]
- 
+
     	
 
-def drawImage(text_en, text_de, text_it, difficulty, maxtxtsize, name, save_location):
+def drawImage(text_en, text_de, difficulty, maxtxtsize, name, save_location):
     # image size
     size = 1000
     # border size, the smaller, the bigger the border is??
@@ -27,9 +27,9 @@ def drawImage(text_en, text_de, text_it, difficulty, maxtxtsize, name, save_loca
     img = Image.new('RGBA', (size, size), (255, 0, 0, 0))
 
     draw = ImageDraw.Draw(img)
-
     # Text Size = max Text size, max Text size is passed to this function (normally 70)
     txtSize = maxtxtsize
+
     #TODO fix this
     fnt = ImageFont.truetype('fonts/' +  font, txtSize)
     # sets  border color for difficulty
@@ -52,8 +52,6 @@ def drawImage(text_en, text_de, text_it, difficulty, maxtxtsize, name, save_loca
         txtSize = int(150 / len(text_en) * 25)
     elif len(text_de) >= 50:
         txtSize = int(150 / len(text_de) * 25)
-    elif len(text_it) >= 50:
-        txtSize = int(150 / len(text_it) * 25)
 
     # clamps max text size to 70, otherwise some text will become larger
     if txtSize > maxtxtsize:
@@ -61,21 +59,17 @@ def drawImage(text_en, text_de, text_it, difficulty, maxtxtsize, name, save_loca
 
 
     # after a set length, add a linebreak. how many word per line depends on text size
-    text_en = "\n".join(textwrap.wrap(text_en, width=int(100 / txtSize * 18)))
-    text_de = "\n".join(textwrap.wrap(text_de, width=int(100 / txtSize * 18)))
-    text_it = "\n".join(textwrap.wrap(text_it, width=int(100 / txtSize * 18)))
+    text_en = "\n".join(textwrap.wrap(text_en, width=int(100 / txtSize * 12)))
+    text_de = "\n".join(textwrap.wrap(text_de, width=int(100 / txtSize * 12)))
 
     # defines height and width of string, taking into account the font
     w_en, h_en = draw.textsize(text_en, font=fnt)
     w_de, h_de = draw.textsize(text_de, font=fnt)
-    w_it, h_it = draw.textsize(text_it, font=fnt)
     # draws the 3 texts. x position is the image size - half of the text size / 2.
     # y positon for german text is centered, other text have a offset from the middle text
     draw.multiline_text(((size - w_en) / 2, (size - h_en) / 2 - h_de - 50), text_en, fill=(0, 0, 0), font=fnt,
                         align="center")
-    draw.multiline_text(((size - w_de) / 2, (size - h_de) / 2), text_de, fill=(0, 0, 0), font=fnt, align="center")
-    draw.multiline_text(((size - w_it) / 2, (size - h_it) / 2 + h_de + 50), text_it, fill=(0, 0, 0), font=fnt,
-                        align="center")
+    draw.multiline_text(((size - w_de) / 2, (size - h_de) / 2 + 50), text_de, fill=(0, 0, 0), font=fnt, align="center")
 
     # tries to apply antialiasing, but it doesnt seem to work
     # TODO revisit antialiasing
@@ -89,7 +83,6 @@ def drawImage(text_en, text_de, text_it, difficulty, maxtxtsize, name, save_loca
     # debug code
     print("text length en: " + str(len(text_en)))
     print("text length de: " + str(len(text_de)))
-    print("text length iz: " + str(len(text_it)))
 
     print("Text Size: " + str(txtSize))
     print("relativ Border thickness: " + str(relBorder))
@@ -120,9 +113,8 @@ layout = [
     [sg.Text('Create Card')],
     [sg.Text('Select Difficulty', size=(15, 1)), sg.Combo(diff, size=(15, 1), readonly=True, default_value='Easy'), sg.Text("Font"), 
         sg.Combo(os.listdir("fonts/"), size=(15, 1), readonly=True, default_value=os.listdir("fonts/")[0])],
-    [sg.Text('German', size=(15, 1)), sg.InputText()],
-    [sg.Text('English', size=(15, 1)), sg.InputText()],
-    [sg.Text('Italian', size=(15, 1)), sg.InputText()],
+    [sg.Text('Language 1', size=(15, 1)), sg.InputText()],
+    [sg.Text('Language 2', size=(15, 1)), sg.InputText()],
     [sg.Text('Choose Filename', size=(15, 1)), sg.InputText(default_text="Filename")],
     [sg.Text('Save Location',  size=(15,1)), sg.Input(default_text=pickle.load(open( "save.p", "rb" ))) ,sg.FolderBrowse()],
     [sg.Save(), sg.Button('Refresh'), sg.Quit()]
@@ -131,25 +123,24 @@ layout = [
 
 # Create the window
 window = sg.Window("DarePonger", layout)
-#drawImage("", "", "", 'Easy', 70, "default", "files/")
 #creates the perview image for the first time
 sg.popup_animated(image_source='files/thumbnail.png', title="Preview",
                   location=(GetSystemMetrics(0) / 5, GetSystemMetrics(1) / 2), keep_on_top=False)
 while True:  # The Event Loop
     event, values = window.read()
-    save = values[6]
+    save = values[5]
     pickle.dump(save, open("save.p", "wb"))
     if event == 'Save':
         font = values[1]
         print(event, values)
-        drawImage(values[2], values[3], values[4], values[0], 70, values[5], values[6])
+        drawImage(values[2], values[3], values[0], 50, values[4], values[5])
         refreshPreview()
         
 
     if event == 'Refresh':
         font = values[1]
         print(event, values)
-        drawImage(values[2], values[3], values[4], values[0], 70, values[5], values[6])
+        drawImage(values[2], values[3], values[0], 50, values[4], values[5])
         refreshPreview()
         
 
